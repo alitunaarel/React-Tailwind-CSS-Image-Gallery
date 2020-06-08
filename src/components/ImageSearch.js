586e96a184784ea925264aa3f07c08b019f81a8c
ali <ali@iMac-de-Elodie.fritz.box>
2020-06-08 12:14:24 +0200

    feature input value empty

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -4,14 +4,15 @@ const ImageSearch = ({ searchText }) => {
   const [text, setText] = useState("");
   const onSubmit = e => {
     e.preventDefault();
-    searchText(text);
+    setText(" ");
+    searchText(text);    
   };
 
   return (
     <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
       <form onSubmit={onSubmit} className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-          <input
+          <input value={text}
             onChange={e => setText(e.target.value)}
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
